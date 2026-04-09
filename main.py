1cc7023058b20efa262e3e386ce27e55e9cfa48b
shalinideviramalingam <shalinidevi.ramalingam@assuredinsights.com>
2026-04-09 16:34:53 +0100

    added new change

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
-from airbyte_cdk.destinations import launch
 from destination_azure_blob import DestinationAzureBlob
-
+ 
 if __name__ == "__main__":
-    destination = DestinationAzureBlob()
-    launch(destination, sys.argv[1:])
+    DestinationAzureBlob().run(sys.argv[1:])
